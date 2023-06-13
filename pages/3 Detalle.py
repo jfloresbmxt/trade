054fce8fec822394eed5e7f5f5c2be783e18ce66
Juan Alejandro Flores Perez <JFLORES@bancomext.gob.mx>
2023-06-13 13:33:10 -0600

    Agrega atenticación en detalle

--- pages/3 Detalle.py
+++ pages/3 Detalle.py
@@ -1,54 +1,77 @@
 import streamlit as st
+from streamlit_folium import st_folium
+import streamlit_authenticator as stauth
 from polars import read_parquet, col
 from secciones.estatal.detalle import detalle_empresas
 from graphs.maps.gen_maps import gen_maps
 import geopandas as gpd
-from streamlit_folium import st_folium
+import yaml
+from yaml.loader import SafeLoader
 
-@st.cache_data
-def get_list():
-    estados = read_parquet("data/lista_estados.parquet")
-    estados = estados.to_series().to_list()
+with open(".config.yaml") as file:
+    config = yaml.load(file, Loader=SafeLoader)
 
-    return estados
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
 
-@st.cache_data
-def get_states_data(state):
-    df_detail = read_parquet("data/detalle/" + state + ".parquet")
-    subsector = (
-        df_detail.select(col("Título"))
-        .unique()
-        .select(col("Título")
-        .apply(lambda x: x.replace("T",""))
-        .apply(lambda x: x.strip())
-        )
-        ).to_series().to_list()
+name, authentication_status, username = authenticator.login('Autenticate', 'main')
 
-    return [df_detail, subsector]
+if authentication_status is False:
+    st.error('Usuario o contraseña son incorrectos')
+if authentication_status is None:
+    st.warning('Para ver esta sección, inicia sesion')
+if authentication_status:
+    authenticator.logout('Cerrar sesión', 'main', key='unique_key')
 
-@st.cache_data
-def plot_map(state):
-    s = gpd.read_parquet("data/map_info/state_" + state + ".parquet")
-    map_info = gpd.read_parquet("data/map_info/cp_" + state + ".parquet")
-    
-    return [s, map_info]
+    @st.cache_data
+    def get_list():
+        estados = read_parquet("data/lista_estados.parquet")
+        estados = estados.to_series().to_list()
 
-st.subheader("Detalle de empresas")
+        return estados
 
-col1, col2 = st.columns(2)
-with col1:
-    opcion = st.selectbox("seleccionar estado", get_list())
-    df_detail, subsector = get_states_data(opcion)
-with col2:
-    opcion2 = st.selectbox("Seleccionar subsector", subsector)
+    @st.cache_data
+    def get_states_data(state):
+        df_detail = read_parquet("data/detalle/" + state + ".parquet")
+        subsector = (
+            df_detail.select(col("Título"))
+            .unique()
+            .select(col("Título")
+            .apply(lambda x: x.replace("T",""))
+            .apply(lambda x: x.strip())
+            )
+            ).to_series().to_list()
+
+        return [df_detail, subsector]
+
+    @st.cache_data
+    def plot_map(state):
+        s = gpd.read_parquet("data/map_info/state_" + state + ".parquet")
+        map_info = gpd.read_parquet("data/map_info/cp_" + state + ".parquet")
+        
+        return [s, map_info]
+
+    st.subheader("Detalle de empresas")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        opcion = st.selectbox("seleccionar estado", get_list())
+        df_detail, subsector = get_states_data(opcion)
+    with col2:
+        opcion2 = st.selectbox("Seleccionar subsector", subsector)
 
 
-detalle_empresas(df_detail, opcion2)
+    detalle_empresas(df_detail, opcion2)
 
-st.subheader("Concentración de empresas")
+    st.subheader("Concentración de empresas")
 
-s, map_info = plot_map(opcion)
+    s, map_info = plot_map(opcion)
 
-m = gen_maps(s, map_info)
+    m = gen_maps(s, map_info)
 
-st_data = st_folium(m, width=500)
+    st_data = st_folium(m, width=500)
